82b713a085808d0a0baf29e69f4b0d305cb1f2ec
Casey Law <caseyjlaw@gmail.com>
2023-03-03 18:17:12 -0500

    polling functions

--- dsa_alert.py
+++ dsa_alert.py
@@ -12,13 +12,13 @@ else:
 ip = '131.215.200.144'  # major
 port = '8001'
 url = f'http://{ip}:{port}'
+path = '/dsa'
 
 
 def get_dsa(password=RELAY_KEY):
     """ Get DSA command from relay.
     """
 
-    path = '/dsa'
     headers = {"Accept": "application/json"}
 
     resp = requests.get(url=url+path, headers=headers, params={'key': RELAY_KEY})
@@ -26,14 +26,13 @@ def get_dsa(password=RELAY_KEY):
     return resp.json()
 
 
-def set_dsa(command, password=RELAY_KEY):
+def set_dsa(command, args={}, password=RELAY_KEY):
     """ Set DSA command to relay.
     """
 
-    path = '/dsa'
     headers = {"Accept": "application/json", 'Content-Type': 'application/json'}
     mjd = time.Time.now().mjd
-    dd = {"command": command, "command_mjd": mjd}
+    dd = {"command": command, "command_mjd": mjd, "args": args}
 
     resp = requests.put(url=url+path, headers=headers, data=json.dumps(dd),
                         params={'key': RELAY_KEY})
@@ -41,11 +40,18 @@ def set_dsa(command, password=RELAY_KEY):
     return resp.status_code
 
 
-def poll_dsa(loop=1):
+def poll_dsa(loop=5):
     """ Poll the relay API for DSA commands.
     """
-    
+
+    dd = get_lwa()
     while True:
         mjd = time.Time.now().mjd
-        dd = get_dsa()
-        
+        dd2 = get_lwa()
+        if dd2["command_mjd"] != dd["command_mjd"]:
+            dd = dd2.copy()
+            print(f"New command: {dd}")
+        else:
+            sleep(loop)
+            continue
+
--- lwa_alert.py
+++ lwa_alert.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from os import environ
+from time import sleep
 from astropy import time
 
 
@@ -12,13 +13,13 @@ else:
 ip = '131.215.200.144'  # major
 port = '8001'
 url = f'http://{ip}:{port}'
+path = '/lwa'
 
 
 def get_lwa(password=RELAY_KEY):
     """ Get LWA command from relay.
     """
 
-    path = '/lwa'
     headers = {"Accept": "application/json"}
 
     resp = requests.get(url=url+path, headers=headers, params={'key': RELAY_KEY})
@@ -30,7 +31,6 @@ def set_lwa(command, args={}, password=RELAY_KEY):
     """ Put LWA command to relay.
     """
 
-    path = '/lwa'
     headers = {"Accept": "application/json", 'Content-Type': 'application/json'}
     mjd = time.Time.now().mjd
     dd = {"command": command, "command_mjd": mjd, "args": args}
@@ -41,11 +41,18 @@ def set_lwa(command, args={}, password=RELAY_KEY):
     return resp.status_code
 
 
-def poll_lwa(loop=1):
+def poll_lwa(loop=5):
     """ Poll the relay API for LWA commands.
     """
-    
+
+    dd = get_lwa()
     while True:
         mjd = time.Time.now().mjd
-        dd = get_lwa()
+        dd2 = get_lwa()
+        if dd2["command_mjd"] != dd["command_mjd"]:
+            dd = dd2.copy()
+            print(f"New command: {dd}")
+        else:
+            sleep(loop)
+            continue
         
