13b800289f265ce7defae1a884eb9dce947c54b7
Nikita Kosogorov <nikita@Nikitas-MacBook-Pro.local>
2023-04-11 18:39:56 -0700

    added buffer trigger to lwa_alert_client

--- ovro_alert/lwa_alert_client.py
+++ ovro_alert/lwa_alert_client.py
@@ -35,4 +35,13 @@ class LWAAlertClient(AlertClient):
         pass
 
 
+if __name__ == '__main__':
+    xhosts = ['lxdlwagpu02']
+    con = control.Controller('proj/lwa-shell/mnc_python/config/lwa_config_calim.yaml', xhosts=xhosts)
+    pipelines = con.pipelines
+    p = pipelines[0]
+    client = LWAAlertClient(p)
+    client.ntime_per_file = 24000
+    client.nfile = 1
+    client.poll(loop=5)
 
