aa9c8d7fc403d17aae6239ceeb1bca385981ac37
Nikita Kosogorov <nikita@Nikitas-MacBook-Pro.local>
2023-05-31 09:11:52 -0700

    Modified ligo alert receiver to save maps and added some logic to it

--- ligo-alert/gcn-buffer_trigger.py
+++ ligo-alert/gcn-buffer_trigger.py
@@ -33,8 +33,9 @@ def process_gcn(payload, root, write=True):
     
     # Respond to both 'test' in case of EarlyWarning alert or 'observation' 
     condition1 = root.attrib['role'] == 'test' and params['AlertType'] == 'EarlyWarning'
-    condition2 = root.attrib['role'] == 'observation' # IMPORTANT! for real observations set to 'observation' 
-    if not (condition1 or condition2):
+    condition2 = root.attrib['role'] == 'observation' # IMPORTANT! for real observations set to 'observation'
+    condition3 = root.attrib['role'] == 'test' # For testing purposes
+    if not (condition1 or condition2 or condition3):
         return
 
     # If event is retracted, print it.
@@ -70,12 +71,41 @@ def process_gcn(payload, root, write=True):
                 f.write("Created at (UTC): " + now.strftime("%Y-%m-%d %H:%M:%S"))
 
         # Send to relay
-        print(f'sending to ligo relay server with role {root.attrib["role"]}')
-        # TODO: logic to catch "test" role for advanced warning
-        # TODO: do we need to select on whether target is up?
-        # TODO: save bayestar region
-        ligoc.set(root.attrib['role'], args={'FAR': params['FAR'], 'BNS': params['BNS'],
-                                             'HasNS': params['HasNS'], 'Terrestrial': params['Terrestrial']})
+        # for EarlyWarning type of Alerts
+        if condition1:
+            print(f'sending EarlyWarning type of alert to ligo relay server with role observation')
+
+            ligoc.set("observation", args={'FAR': params['FAR'], 'BNS': params['BNS'],
+                                                 'HasNS': params['HasNS'], 'Terrestrial': params['Terrestrial']})
+        # in case of real observation
+        elif condition2:
+
+            print(f'sending to ligo relay server with role {root.attrib["role"]}')
+
+            ligoc.set(root.attrib['role'], args={'FAR': params['FAR'], 'BNS': params['BNS'],
+                                                     'HasNS': params['HasNS'], 'Terrestrial': params['Terrestrial']})
+        # for testing, send with nsamp parameter
+        elif condition3:
+
+            print(f'sending to ligo relay server with role {root.attrib["role"]}')
+
+            ligoc.set(root.attrib['role'], args={'FAR': params['FAR'], 'BNS': params['BNS'],
+                                                 'HasNS': params['HasNS'], 'Terrestrial': params['Terrestrial'],
+                                                 'nsamp': 24000})
+
+
+        # Save bayestar map
+        if 'skymap_fits' in params:
+            # Read the HEALPix sky map and the FITS header.
+            skymap, _ = ligo.skymap.io.read_sky_map(params['skymap_fits'])
+
+            # Write the skymap to a file
+            skymap_file_name = params['GraceID'] + '_skymap.fits'
+            ligo.skymap.io.write_sky_map(skymap_file_name, skymap, overwrite=True)
+
+            # TODO: logic to catch "test" role for advanced warning
+            # TODO: do we need to select on whether target is up?
+
     else:
         print(f'Event did not pass selection: FAR {params["FAR"]}, BNS {params["BNS"]}, Terrestrial {params["Terrestrial"]}.')
             
