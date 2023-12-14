0ccd15e23e8f20f8c3a6002e80a2b16f61063759
Casey Law <caseyjlaw@gmail.com>
2023-12-14 10:10:21 -0500

    parsing and printing gcn info

--- gcn-alert/gcn_receiver.py
+++ gcn-alert/gcn_receiver.py
@@ -7,21 +7,33 @@ gc = alert_client.AlertClient('gcn')
 
 # Define your custom handler here.
 @gcn.include_notice_types(
-    gcn.notice_types.SWIFT_BAT_GRB_POS_ACK
+    gcn.notice_types.SWIFT_BAT_GRB_POS_ACK,
+    gcn.notice_types.SWIFT_BAT_MONITOR,  # test
+    gcn.notice_types.FERMI_GBM_FLT_POS,  # test
     )
 def handler(payload, root):
-    # Look up right ascension, declination, and error radius fields.
+    # parse
     ve = voeventparse.loads(payload)
-    print(f'test {ve}')
-    pos2d = root.find('.//{*}Position2D')
-    ra = float(pos2d.find('.//{*}C1').text)
-    dec = float(pos2d.find('.//{*}C2').text)
-    radius = float(pos2d.find('.//{*}Error2Radius').text)
+
+    # get values
+    pos = voeventparse.convenience.get_event_position(ve)
+    ra = pos.ra
+    dec = pos.dec
+    radius = pos.err
+    dt = voeventparse.convenience.get_event_time_as_utc(ve)
+    toplevel_params = voeventparse.get_toplevel_params(ve)
+    grouped_params = voeventparse.convenience.get_grouped_params(ve)
+    author = ve.Who.Author.shortName
 
     # Print.
-    print('ra = {:g}, dec={:g}, radius={:g}'.format(ra, dec, radius))
+    print(f'Event from {author} at {dt.isoformat()}: RA, Dec = ({ra}, {dec}}, radius={radius}.')
+    print(f'Toplevel params: {toplevel_params.keys()}')
+    print(f'Grouped params: {grouped_params.keys()}')
+
+    # send it
     args = {'duration': 1800, 'position': f'{ra},{dec},{radius}'}
-    gc.set('observation', args)
+    role = ve.attrib['role']
+    gc.set(role, args)
 
 # Listen for VOEvents until killed with Control-C.
 gcn.listen(handler=handler)
